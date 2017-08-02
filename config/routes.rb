ac5be9c40bb629b98a1884b5497b8efb6542ce2c
Policonickolu <heidy.benyahia@gmail.com>
2017-08-02 16:38:09 +0200

    css layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :users
   root 'static_pages#home'
-  get 'home', to: 'static_pages#home'
+  get '/signup', to: 'users#new'
+  resources :users
 
 end
