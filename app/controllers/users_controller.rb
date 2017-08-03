0858aa82d5eb576739292a403ba9e1d2d37ee607
Policonickolu <heidy.benyahia@gmail.com>
2017-08-03 16:06:49 +0200

    login logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-
   end
 
   # GET /users/new
@@ -29,6 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        log_in @user
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
