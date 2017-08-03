a303a7a712cd50028a1b4fb2ac41d84229030218
Policonickolu <heidy.benyahia@gmail.com>
2017-08-03 13:40:54 +0200

    SSL and Puma

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, name: @user.name } }
+      post users_url, params: { user: { email: "abc@ee.fr", name: "fifou", password: "fooooo", password_confirmation: "fooooo" } }
     end
 
     assert_redirected_to user_url(User.last)
@@ -34,7 +34,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, name: @user.name } }
+    patch user_url(@user), params: { user: { email: "abc@ee.fr", name: "yoyo", password: "fooooo", password_confirmation: "fooooo" } }
     assert_redirected_to user_url(@user)
   end
 
